f54fea42979ea6445d1eb16f7b5833da1a1a4967
ValentinaLV <tina.lysenok@gmail.com>
2020-05-15 23:36:22 +0300

    minor update in settings

--- news_board/settings.py
+++ news_board/settings.py
@@ -122,9 +122,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 #for heroku app
 django_heroku.settings(locals())
